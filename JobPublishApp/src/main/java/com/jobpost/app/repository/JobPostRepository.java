b21bb8dc1bb2996c4a73468edc46decca31a3189
umang111 <51570750+umang111@users.noreply.github.com>
2022-12-22 19:29:47 +0530

    Delete JobPublishApp directory

--- JobPublishApp/src/main/java/com/jobpost/app/repository/JobPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobpost.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jobpost.app.entity.JobPost;
-
-public interface JobPostRepository extends JpaRepository<JobPost, Integer> {
-
-}
